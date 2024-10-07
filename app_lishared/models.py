b58d5e7313be9fb1e08e1f2d230f162043990fd9
uGonzaguinha <gustavogsantos2013@gmail.com>
2024-10-06 22:21:48 -0300

    criação do cadastramento

--- app_lishared/models.py
+++ app_lishared/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("O usuário deve ter um endereço de e-mail")
+        if not username:
+            raise ValueError("O usuário deve ter um nome de usuário")
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
--- app_lishared/views.py
+++ app_lishared/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
+def cadastro(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirmPassword']
+        
+        if password != confirm_password:
+            messages.error(request, 'As senhas não coincidem.')
+            return render(request, 'cadastro.html', {'username': username, 'email': email})
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Nome de usuário já existe.')
+            return render(request, 'cadastro.html', {'username': username, 'email': email})
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'E-mail já cadastrado.')
+            return render(request, 'cadastro.html', {'username': username, 'email': email})
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        messages.success(request, 'Cadastro realizado com sucesso!')
+        return redirect('cadastro')
+    
+    return render(request, 'cadastro.html')
